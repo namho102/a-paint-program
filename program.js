83c5e30ea28bf70cf4b5f8a441a2a0b776649e70
Hoang Nam <namhoang.td09@gmail.com>
2014-11-12 21:36:02 +0700

    Update program.js

--- program.js
+++ program.js
@@ -215,14 +215,11 @@ function rectangleFrom(a, b) {
         width: Math.abs(a.x - b.x),
         height: Math.abs(a.y - b.y)
     };
-}
+  }
 
-tools.Rectangle = function(event, cx) {
+  tools.Rectangle = function(event, cx) {
     var relativeStart = relativePos(event, cx.canvas);
-    var pageStart = {
-        x: event.pageX,
-        y: event.pageY
-    };
+    var pageStart = {x: event.pageX, y: event.pageY};
 
     var trackingNode = document.createElement("div");
     trackingNode.style.position = "absolute";
@@ -230,14 +227,18 @@ tools.Rectangle = function(event, cx) {
     document.body.appendChild(trackingNode);
 
     trackDrag(function(event) {
-        var rect = rectangleFrom(pageStart, {x: event.pageX, y: event.pageY});
-        trackingNode.style.left = rect.left + "px";
-        trackingNode.style.top = rect.top + "px";
-        trackingNode.style.width = rect.width + "px";
-        trackingNode.style.height = rect.height + "px";
+      var rect = rectangleFrom(pageStart, {
+          x: event.pageX, 
+          y: event.pageY
+        });
+        
+      trackingNode.style.left = rect.left + "px";
+      trackingNode.style.top = rect.top + "px";
+      trackingNode.style.width = rect.width + "px";
+      trackingNode.style.height = rect.height + "px";
     }, function(event) {
-        var rect = rectangleFrom(relativeStart, relativePos(event, cx.canvas));
-        cx.fillRect(rect.left, rect.top, rect.width, rect.height);
-        document.body.removeChild(trackingNode);
-    })
-};
+      var rect = rectangleFrom(relativeStart, relativePos(event, cx.canvas));
+      cx.fillRect(rect.left, rect.top, rect.width, rect.height);
+      document.body.removeChild(trackingNode);
+    });
+  };
